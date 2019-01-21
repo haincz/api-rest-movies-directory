61c4546de0e7dedef7aaded8429ad7812675b9b0
Mateusz Mikoś <haincz@gmail.com>
2019-01-21 23:02:50 +0100

    code cleaning

--- api/comments/commentsRouts.js
+++ api/comments/commentsRouts.js
@@ -8,34 +8,13 @@ router.get('/', (req, res) => {
 
 	comments.commnentsList()
 			.then((comments) => res.json(comments))
-
-
-	// comments.commnentsList((err, comments) => {
-	// 	res.json(comments);
-	// });
 });
 
 router.post('/', (req, res) => {
 
-	//in future here should be checking is ID exist in dataBase (use: movies.getFilmById)
-	// if(req.body.hasOwnProperty("film_id") !== true) {
-	// 	res.status(404).send({"status":"False. Correct film_id is required"})
-	// } else {
-
-		comments.addCommnent(req.body)
-				.then((data) => res.json(data))
-				.catch((error) => res.status(400).send(error));
-
-
-		// comments.addCommnent(req.body, (err, data) => {
-		// 	if (err){
-		// 		res.status(400).send(err)
-		// 	} else {
-		// 	res.json(data);
-		// 	}		
-		// });
-	
-	// };
+	comments.addCommnent(req.body)
+		.then((data) => res.json(data))
+		.catch((error) => res.status(400).send(error));
 
 });
 
@@ -52,23 +31,8 @@ router.get('/moviecomments/:id', (req, res) => {
 	}
 
 	comments.getCommnentsByFilmId(req.params.id)
-			.then(send)
-			.catch(send)
-
-
-
-
-	// 	if (err) {
-	// 		res.status(404).send({"error":"Not Found"});
-	// 	} else {
-	// 		if (!comments.length){
-	// 			res.status(404).send({"error":"Not Found"});
-	// 		} else {
-	// 			res.json(comments);
-	// 		};
-	// 	};
-	// });
-
+		.then(send)
+		.catch(send)
 });
 
 
